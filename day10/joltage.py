b740f998b7859071bd62adf4870f63036c54b477
OwenFeik <owenhfeik@gmail.com>
2020-12-19 12:02:01 +1100

    day 10 part 2

--- day10/joltage.py
+++ day10/joltage.py
@@ -8,13 +8,37 @@ joltages.append(0)
 joltages.append(max(joltages) + 3)
 joltages = sorted(joltages)
 
-print(joltages)
 
-deltas = {}
-for i in range(1, len(joltages)):
-    delta = joltages[i] - joltages[i - 1]
-    if delta not in deltas:
-        deltas[delta] = 0
-    deltas[delta] += 1
+def part_one():
+    deltas = {}
+    for i in range(1, len(joltages)):
+        delta = joltages[i] - joltages[i - 1]
+        if delta not in deltas:
+            deltas[delta] = 0
+        deltas[delta] += 1
 
-print(deltas)
+    print(deltas)
+
+def part_two():
+    n = len(joltages) - 1
+
+    def dfs(i, memo={}):
+        if i in memo:
+            return memo[i]
+            
+        solns = 0
+
+        if i == n:
+            return 1
+
+        for j in range(i + 1, len(joltages)):
+            if joltages[j] - joltages[i] in [1, 2, 3]:
+                print(j)
+                solns += dfs(j)
+
+        memo[i] = solns
+        return solns
+
+    print(dfs(0))
+
+part_two()
