74cbd2007cf2f2de20e87c46143bcc7d53a518ca
OwenFeik <owenhfeik@gmail.com>
2021-12-08 16:39:08 +1100

    start thinking

--- 2021/day7/day7.hs
+++ 2021/day7/day7.hs
@@ -9,9 +9,24 @@ replaceCommas str =
     in
         map repl str
 
+median :: [Int] -> Int
+median nums
+    | even l = div (nums !! m + (nums !! m + 1)) 2
+    | otherwise = nums !! m
+    where
+        l = length nums
+        m = div l 2
+
+cost :: [Int] -> Int -> Int
+cost crabs goal = sum $ map (\c -> abs $ goal - c) crabs
+
+part1 :: [Int] -> Int
+part1 crabs =
+    let goal = median crabs in cost crabs goal
+
 main :: IO ()
 main = do
     input <- getContents
-    let initialState = map (\n -> read n :: Int) $ words $ replaceCommas input
-    putStrLn $ "Part 1: " ++ show 0
+    let crabs = map (\n -> read n :: Int) $ words $ replaceCommas input
+    putStrLn $ "Part 1: " ++ show (part1 crabs)
     putStrLn $ "Part 2: " ++ show 0
