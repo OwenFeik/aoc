ddab3657719fed7735b26b1586533c43169642ff
Owen Feik <owenhfeik@gmail.com>
2024-12-02 11:54:48 +1100

    tidy up day 1 solution

--- 2024/day1/day1.hs
+++ 2024/day1/day1.hs
@@ -7,23 +7,21 @@ readFileLines fp = do
     text <- hGetContents handle
     return (lines text)
 
-parse :: [String] -> [(Int, Int)]
-parse = map ((\l -> (read (head l), read (l !! 1))) . words)
+parse :: [String] -> ([Int], [Int])
+parse lines =
+    let pairs = map ((\l -> (read (head l), read (l !! 1))) . words) lines
+    in (map fst pairs, map snd pairs)
 
-part1 :: [(Int, Int)] -> IO ()
-part1 input =
-    let lefts = sort (map fst input)
-        rights = sort (map snd input)
-    in print $ sum $ zipWith (\ a b -> abs (a - b)) lefts rights
+part1 :: [Int] -> [Int] -> IO ()
+part1 lefts rights =
+    print $ sum $ zipWith (\a b -> abs (a - b)) (sort lefts) (sort rights)
 
-part2 :: [(Int, Int)] -> IO ()
-part2 input = 
-    let lefts = sort (map fst input)
-        rights = sort (map snd input)
-    in print $ sum $ map (\n -> n * length (filter (== n) rights)) lefts
+part2 :: [Int] -> [Int] -> IO ()
+part2 lefts rights = 
+    print $ sum $ map (\n -> n * length (filter (== n) rights)) lefts
 
 main = do
     lines <- readFileLines "input.txt"
-    let input = parse lines
-    part1 input
-    part2 input
+    let (lefts, rights) = parse lines
+    part1 lefts rights
+    part2 lefts rights
