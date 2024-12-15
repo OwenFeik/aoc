f83608fe18a76e3b253e31ea0f9065e2705c2a37
Owen Feik <owenhfeik@gmail.com>
2024-12-16 08:14:06 +1100

    aoc 2024 day 13

---
+++ 2024/day13/day13.hs
@@ -0,0 +1,103 @@
+import Data.Char (isDigit)
+import Data.Maybe (fromMaybe)
+import Data.Map (Map, (!), empty, insert, lookup, member)
+import System.IO
+import Debug.Trace
+
+type Button = (Int, Int)
+type Pos = (Int, Int)
+type Machine = (Button, Button, Pos)
+
+readNum :: String -> (String, Int)
+readNum string = let digits = takeWhile isDigit string in
+    (drop (length digits) string, read digits)
+
+parseButton :: String -> Button
+parseButton line =
+    let rest = drop (length "Button A: X+") line
+        (rest', dx) = readNum rest
+        (_, dy) = readNum (drop (length ", Y+") rest') in (dx, dy)
+
+parsePrize :: String -> Pos
+parsePrize line = 
+    let rest = drop (length "Prize: X=") line
+        (rest', x) = readNum rest
+        (_, y) = readNum (drop (length ", Y=") rest') in (x, y)
+
+parseMachine :: String -> String -> String -> Machine
+parseMachine ba bb pz = (parseButton ba, parseButton bb, parsePrize pz)
+
+parseMachines :: [String] -> [Machine]
+parseMachines [] = []
+parseMachines (ba:bb:pz:blank:lines) =
+    (parseMachine ba bb pz):(parseMachines lines)
+parseMachines (ba:bb:pz:[]) = [parseMachine ba bb pz]
+
+readInput :: String -> IO [Machine]
+readInput fp = do
+    handle <- openFile fp ReadMode
+    text <- hGetContents handle
+    return $ parseMachines $ lines text
+
+pressButton :: Pos -> Button -> Pos
+pressButton (x, y) (dx, dy) = (x + dx, y + dy)
+
+type Memo = Map Pos (Maybe Int)
+
+minCostFrom :: Memo -> Machine -> Pos -> (Memo, Maybe Int)
+minCostFrom memo (ba, bb, pz) at =
+    let ata = pressButton at ba
+        atb = pressButton at bb
+        memo' = minCostMemo memo (ba, bb, pz) ata
+        memo'' = minCostMemo memo' (ba, bb, pz) atb
+        a = memo'' ! ata
+        b = memo'' ! atb in case (a, b) of
+            (Just ca, Just cb) -> (memo'', Just (min (ca + 3) (cb + 1)))
+            (Just ca, Nothing) -> (memo'', Just (ca + 3))
+            (Nothing, Just cb) -> (memo'', Just (cb + 1))
+            (Nothing, Nothing) -> (memo'', Nothing)
+
+minCostMemo :: Memo -> Machine -> Pos -> Memo
+minCostMemo memo (ba, bb, pz) at
+    | at == pz = insert at (Just 0) memo
+    | (fst at) > (fst pz) || (snd at) > (snd pz) = insert at Nothing memo
+    | member at memo = memo
+    | otherwise = let (memo', cost) = minCostFrom memo (ba, bb, pz) at in
+        insert at cost memo'
+
+minCost :: Machine -> Maybe Int
+minCost machine = minCostMemo empty machine (0, 0) ! (0, 0)
+
+part1 :: [Machine] -> IO ()
+part1 machines = print . sum $ map (fromMaybe 0 . minCost) machines
+
+toDouble :: Int -> Double
+toDouble v = fromIntegral v
+
+isInteger :: Double -> Bool
+isInteger v = fromInteger (round v) == v
+
+minCost2 :: Machine -> Maybe Int
+minCost2 (ba, bb, pz) =
+    let dxa = toDouble (fst ba)
+        dya = toDouble (snd ba)
+        dxb = toDouble (fst bb)
+        dyb = toDouble (snd bb)
+        x = toDouble (fst pz)
+        y = toDouble (snd pz)
+        b = (dxa * y - dya * x) / (dxa * dyb - dya * dxb)
+        a = (x - (b * dxb)) / dxa in if isInteger a && isInteger b
+            then Just (3 * (round a) + round b)
+            else Nothing 
+
+adjust :: Int
+adjust = 10000000000000
+
+part2 :: [Machine] -> IO ()
+part2 machines = print . sum $ map (fromMaybe 0 . minCost2) $
+    map (\(ba, bb, (x, y)) -> (ba, bb, (x + adjust, y + adjust))) machines
+
+main = do
+    input <- readInput "input.txt"
+    part1 input
+    part2 input
